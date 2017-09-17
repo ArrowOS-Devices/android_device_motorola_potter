2dfaf12dd4bca68e8cd0a000422c3dbd96b7432b
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    potter: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- device.mk
+++ device.mk
@@ -388,6 +388,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.potter
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
