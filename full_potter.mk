f8d23465cf2b413797f0f0c0a45e0b40ae85ca31
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-20 00:20:34 -0200

    potter: Disable dexpreopt for some modules

--- full_potter.mk
+++ full_potter.mk
@@ -16,6 +16,7 @@
 
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
+$(call inherit-product, device/motorola/potter/dexpreopt.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
