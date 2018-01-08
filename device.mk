ee54b947f7eb81223cdd74b269c0adb44d6eb62b
Vachounet <vachounet@live.fr>
2018-01-08 09:55:39 +0100

    potter: enable binderized bluetooth

--- device.mk
+++ device.mk
@@ -91,7 +91,8 @@ PRODUCT_COPY_FILES +=  \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Browser
 PRODUCT_PACKAGES += \
