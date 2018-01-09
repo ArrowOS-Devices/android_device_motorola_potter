ea385ac75119910fca9825846e984041271b0e41
Vachounet <vachounet@live.fr>
2018-01-09 10:39:41 +0100

    Revert "potter: Switch to xml audio effects"
    (reverted from commit 43b7dcfaacf27fa38f265e247d88ba36ce54e56f)

--- device.mk
+++ device.mk
@@ -78,7 +78,7 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/vendor/etc/audio_platform_info_extcodec.xml \
