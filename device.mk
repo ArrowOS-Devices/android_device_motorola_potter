8d791ff68adbdfed9217185c5c49f6ddd4096c6b
Vachounet <vachounet@live.fr>
2018-01-11 15:06:14 +0100

    potter: shims cleanup
    Change-Id: Ideafbadf8d51c94b7505201239e32435dc3ea54f

--- device.mk
+++ device.mk
@@ -198,10 +198,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
-# IMS
-PRODUCT_PACKAGES += \
-    libshim_ims
-
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
@@ -329,7 +325,6 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
-    libshim_ril \
     libprotobuf-cpp-full \
     libxml2
 
