6d56a0f0378fa4f10cc15e2fa7f31362cf15d623
Vachounet <vachounet@live.fr>
2018-01-16 17:30:20 +0100

    potter: update post-install script
    * Permissions moved to vendor
    * Fix deep sleep issue on variants without NFC
    
    Change-Id: Ie5a24a3debe638a3f491d12aa62bf1775b955972

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -4,19 +4,19 @@ sku=`getprop ro.boot.hardware.sku`
 
 if [ "$sku" = "XT1687" ]; then
     # XT1687 doesn't have NFC chip
-    rm /system/etc/permissions/android.hardware.nfc.xml
-    rm /system/etc/permissions/android.hardware.nfc.hce.xml
-    rm /system/etc/permissions/com.android.nfc_extras.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 else
     # Only XT1687 variant got a compass
-    rm /system/etc/permissions/android.hardware.sensor.compass.xml
+    rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
 
 if ! [ "$sku" = "XT1683" ]; then
     # Others variants doesn't have DTV support
-    rm /system/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /system/etc/permissions/mot_dtv_permissions.xml
+    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
     rm /system/lib/libdtvtuner.so
     rm /system/lib64/libdtvtuner.so
     rm /system/vendor/lib/libdtvhal.so
