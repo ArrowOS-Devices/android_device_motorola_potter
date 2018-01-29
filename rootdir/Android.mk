496cefb7e6bdc0e485e9fef9ac8cdab4ca9d1746
Vachounet <vachounet@live.fr>
2018-01-29 20:17:10 +0100

    potter: fix FMRadio after enforcing
    * Use oss service
    * Set proper context
    
    Change-Id: I7bd58339bf2d9d37847961f9260eead785c14c5a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -26,16 +26,7 @@ LOCAL_SRC_FILES    := etc/wlan_carrier_bin.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.fm.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
 # Init scripts
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
