18365480ac55633342f70cbb29632610b4a9a09d
Kevin Rocard <krocard@google.com>
2018-03-06 18:31:18 -0800

    potter: Migrate to audio V4
    This is needed for the microphone characteristic API support.
    
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- device.mk
+++ device.mk
@@ -82,8 +82,8 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES +=  \
