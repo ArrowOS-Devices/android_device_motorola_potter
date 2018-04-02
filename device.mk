a114e7cd6f40a1e60557c2aa27ca073209023390
Vachounet <vachounet@live.fr>
2018-04-02 10:08:42 +0200

    potter: build libqdMetaData.system
    Change-Id: Ia5ad7e28569253d267cead4dbc85bc4fc202d7c2

--- device.mk
+++ device.mk
@@ -148,7 +148,8 @@ PRODUCT_PACKAGES += \
     memtrack.msm8953 \
     libgenlock \
     libtinyxml \
-    libdisplayconfig
+    libdisplayconfig \
+    libqdMetaData.system
 
 PRODUCT_PACKAGES += android.hardware.media.omx
 
