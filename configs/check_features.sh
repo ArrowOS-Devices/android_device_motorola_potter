4e9882af0ad719617e6488596917d897829336e5
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-06-03 23:21:22 -0300

    potter: Update check_features to match newer DTV blobs

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -17,10 +17,16 @@ if ! [ "$sku" = "XT1683" ]; then
     # Others variants doesn't have DTV support
     rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
     rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
+    rm /system/vendor/bin/hw/motorola.hardware.tv@1.0-service
+    rm /system/vendor/etc/init/motorola.hardware.tv@1.0-service.rc
     rm /system/vendor/lib/libdtvtuner.so
+    rm /system/vendor/lib/hw/motorola.hardware.tv@1.0-impl.so
+    rm /system/vendor/lib/motorola.hardware.tv@1.0.so
+    rm /system/vendor/lib/motorola.hardware.tv@1.0_vendor.so
     rm /system/vendor/lib64/libdtvtuner.so
-    rm /system/vendor/lib/libdtvhal.so
-    rm /system/vendor/lib64/libdtvhal.so
+    rm /system/vendor/lib64/hw/motorola.hardware.tv@1.0-impl.so
+    rm /system/vendor/lib64/motorola.hardware.tv@1.0.so
+    rm /system/vendor/lib64/motorola.hardware.tv@1.0_vendor.so
     rm -r /system/vendor/app/DTVPlayer
     rm -r /system/vendor/app/DTVService
 fi
