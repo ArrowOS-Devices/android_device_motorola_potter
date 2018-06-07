8e23e7ba6197f621a789c1a867c6b5a5dce40f83
Vachounet <vachounet@live.fr>
2018-06-07 13:15:15 +0200

    potter: clean-up camera shims

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,9 +225,6 @@ MOT_SENSOR_HUB_FEATURE_GR := true
 # Shim
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/adspd|libshim_adsp.so \
-    /system/lib/lib_motsensorlistener.so|libsensor.so \
-    /system/lib/libjustshoot.so|libshims_camera.so \
-    /system/lib/hw/camera.msm8953.so|libshim_camera_hal.so \
     /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so
 
 # Wifi
