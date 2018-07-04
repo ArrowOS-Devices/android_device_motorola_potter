PixelExperience-Devices/device_motorola_potter

potter: Use GoogleCameraMod instead of Snap
* Based on Arnova_V6bt5

My changes:

* Removed panorama, photo sphere and slowmo from left Menu, because with our current blobs lead to crash

Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

GoogleCameraMod/Android.mk
@@ -1,0 +1,10 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCameraMod
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleCameraMod/Arnova_V6bt5_mod_jhenrique09.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := Camera2
+include $(BUILD_PREBUILT)

ArrowOS-Devices/android_device_motorola_potter

GoogleCameraMod/Android.mk
@@ -1,0 +1,10 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCameraMod
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleCameraMod/Arnova_V6bt5_mod_jhenrique09.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := Snap
+include $(BUILD_PREBUILT)