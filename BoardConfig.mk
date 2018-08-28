d4f85e6b7ebb4537b13a84231e196fe278149f2b
kubersharma001 <kubersharma001@gmail.com>
2018-08-28 21:17:01 +0530

    potter: disable dexpreopt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,14 +51,7 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_OTA_ASSERT_DEVICE := potter,potter_retail
 
 # Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT := false
 
 # GPS
 TARGET_NO_RPC := true
