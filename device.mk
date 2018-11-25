4ea0a60f910eca81d895e6aa4a5f8e458410a8c3
William Bellavance <flintman@flintmancomputers.com>
2018-11-25 09:58:39 -0500

    potter: fix up DEVICE_PACKAGE_OVERLAYS
    Change-Id: Ia660e06fb192517a718b50b9726b708bcf7183f4

--- device.mk
+++ device.mk
@@ -17,10 +17,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
