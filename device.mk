a7a530da9e438ab75a5d496d07d1f63dda8ff2d1
Alberto97 <albertop2197@gmail.com>
2019-01-08 17:50:39 +0100

    potter: Pie RIL upgrade

--- device.mk
+++ device.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libprotobuf-cpp-full \
     libxml2 \
-    android.hardware.radio@1.0
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
