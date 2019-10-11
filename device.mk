ac53d5f54862d74472e9bc934eac34f76d70b05f
kubersharma001 <kubersharma001@gmail.com>
2019-10-11 14:14:00 +0530

    potter: No need to add CarrierConfig in PRODUCT_PACKAGES
    Built by default
    
    Change-Id: Ia20f3fd8c2df8beb50dfc40ff9153723e5999660

--- device.mk
+++ device.mk
@@ -118,10 +118,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/vfwconfig.json:system/etc/camera/vfwconfig.json \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
-# CarrierConfig
-PRODUCT_PACKAGES += \
-    CarrierConfig
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8953 \
