28b4480bf39ba74ca18de682317e2b3db580c2f0
kubersharma001 <kubersharma001@gmail.com>
2019-10-11 22:16:56 +0530

    potter:[DNM] Add few ril/ims props
    Change-Id: Ia8833530ebeb0e7121e223500f6385f3fb0b8767

--- device_props.mk
+++ device_props.mk
@@ -190,6 +190,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.volte_enabled_by_hw=1 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.ims_volte_enable=1
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.voice_on_lte=1 \
+    persist.vendor.radio.calls.on.ims=1 \
 
 # Location
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
