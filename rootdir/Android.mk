79c3370e02db293aff3f9b01d4c6a01c422911f7
Anish Pratheepan <42488259+ultranoob-5@users.noreply.github.com>
2019-10-15 22:41:15 +0530

    potter: rootdir: Drop eng tag for init.potter.rc
    Change-Id: Iacbd2bf16dd897347f51c6e189dd796c6f4dc57f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -45,7 +45,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.potter.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.potter.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
