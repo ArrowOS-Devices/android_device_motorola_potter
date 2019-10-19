7ab9afcf2e6a828cc73e0935f953b5f876874c62
kubersharma001 <kubersharma001@gmail.com>
2019-10-19 20:07:37 +0200

    potter: Drop data-ipa-cfg-mgr
    Change-Id: I3e594b2954f8a32a7f83c53790ec28db36d824ec

--- device.mk
+++ device.mk
@@ -202,11 +202,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
 
-# IPACM
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
