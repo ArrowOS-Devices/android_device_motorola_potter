298c6ffc4cea069888f776a0241b5211bca1596c
kubersharma001 <kubersharma001@gmail.com>
2019-11-04 21:33:18 +0530

    potter: [DNM] Switch to 32bit for few camera packages
     and build few additional camera packages
    
    Change-Id: I526e81e47517673a09f9a2960827541b1d703ddb

--- device.mk
+++ device.mk
@@ -108,8 +108,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl:32 \
+    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     libjustshoot_shim \
