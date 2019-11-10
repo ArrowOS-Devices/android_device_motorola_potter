2430edf4d21bdcb504d2aceffe5c7b2ffa74d8f1
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-11-10 16:06:54 -0200

    potter: Switch to QSEE implementation
    Change-Id: I50e0551c0c4d53f4c0fdbbd3349a5afc4207e488

--- device.mk
+++ device.mk
@@ -213,7 +213,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # IRSC
 PRODUCT_COPY_FILES += \
