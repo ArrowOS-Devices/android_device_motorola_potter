9f92bb307a0a06efe17e82fbd03a5ebdf5b33f03
Demon000 <Demon000@users.noreply.github.com>
2019-11-12 01:27:59 +0100

    potter: Convert lights HAL to native binderized HAL [Adapted from libra for griffin]
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: ZJRDroid <priv.zjr@gmail.com>

--- device.mk
+++ device.mk
@@ -251,8 +251,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    android.hardware.light@2.0-service.potter
 
 # MotoActions
 PRODUCT_PACKAGES += \
