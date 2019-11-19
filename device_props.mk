94b303e39a21d13f60032cc903d9d8e964cb7b22
kubersharma001 <kubersharma001@gmail.com>
2019-11-19 12:23:15 +0530

    potter: Fixup nitz props

--- device_props.mk
+++ device_props.mk
@@ -196,15 +196,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rild.nitz_plmn= \
-    persist.rild.nitz_long_ons_0= \
-    persist.rild.nitz_long_ons_1= \
-    persist.rild.nitz_long_ons_2= \
-    persist.rild.nitz_long_ons_3= \
-    persist.rild.nitz_short_ons_0= \
-    persist.rild.nitz_short_ons_1= \
-    persist.rild.nitz_short_ons_2= \
-    persist.rild.nitz_short_ons_3=
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
 
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
