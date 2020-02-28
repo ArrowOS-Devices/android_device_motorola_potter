8f5add524fa0b484f1c932a7f0d6036335e2d484
kubersharma001 <kubersharma001@gmail.com>
2020-02-28 06:46:18 +0100

    potter: Do not clean vendor when extracting by default.
    Change-Id: I05bb0bcd11a9433125602e32fa808960ac4ef8a8

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
-# Copyright (C) 2019 ArrowOS
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,7 +35,7 @@ fi
 . "$HELPER"
 
 # Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
+CLEAN_VENDOR=false
 
 while [ "$1" != "" ]; do
     case $1 in
