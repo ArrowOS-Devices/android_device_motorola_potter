67e1a4d7554024800b776de68a97dbb8511606cc
kubersharma001 <kubersharma001@gmail.com>
2020-03-19 17:45:38 +0530

    potter: Set Boot Anim res.

--- arrow_potter.mk
+++ arrow_potter.mk
@@ -29,6 +29,7 @@ $(call inherit-product, device/motorola/potter/device.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
